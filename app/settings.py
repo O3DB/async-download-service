a14d99590f7e275dc86b5a9c0feba1248b1279d6
Oleg <orlov.oficial@gmail.com>
2020-04-10 15:47:01 +0300

    Replace archive folder

--- app/settings.py
+++ app/settings.py
@@ -7,7 +7,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class CONFIG:
     SERVICE_NAME = 'MarriageAgency'
     SERVICE_PORT = int(os.environ.get('SERVICE_PORT', 5000))
-    MEDIA_URI = os.path.join(basedir, os.getenv('PHOTOS_PATH', 'static/test_photos'))
+    MEDIA_URI = os.path.join(basedir, os.getenv('PHOTOS_PATH', 'archive'))
     TEMPLATES_URI = os.path.join(basedir, 'templates')
     DELAY = int(os.getenv('DELAY', 0))
     LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
